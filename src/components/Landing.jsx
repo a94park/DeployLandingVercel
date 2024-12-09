c3adfc6d9f14491fb696ba42d78854622ef06451
Abraham Park <159606467+a94park@users.noreply.github.com>
2024-12-09 21:41:03 +0900

    flappy accident

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,7 +2,7 @@ import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 import ViewDownloadCV from "./ViewDownloadCV.jsx";
 import SkillsCarousel2 from "./SkillsCarousel2.jsx";
 import Projects from "./Projects.jsx";
-import Flappy from "./Flappy.jsx";
+// import Flappy from "./Flappy.jsx";
 // import Suggestions from "./Suggestions.jsx";
 import "./Landing.scss";
 const Landing = () => {
@@ -17,9 +17,9 @@ const Landing = () => {
         </h3>
         <h3>Currently learning C# with Unity.</h3>
       </div>
-      <div className="flappy">
+      {/* <div className="flappy">
         <Flappy />
-      </div>
+      </div> */}
 
       {/* Contact Section */}
       <div className="contact-section">
